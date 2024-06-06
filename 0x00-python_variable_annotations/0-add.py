ed67e5519e4312c0f786faf3ae770cfc8598996a
solomonchidera <solochidera10@gmail.com>
2024-06-06 22:58:08 +0100

    A python funtion that uses the type annotation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
 """
-0-add.py
-
-This module provides addition operation.
-
-Functions:
-- add(a: float, b: float) -> float: Add two floats and return the result.
+Module Docs
 """
 
 
 def add(a: float, b: float) -> float:
-    '''
+    """
     Multiply two floats and return the result.
 
     Parameters:
-    a: float
-    b: float
+    - a: float
+    - b: float
 
     Return Value:
     float: the result of two addition of floats
-    '''
+    """
     return a + b
