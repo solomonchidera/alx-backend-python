0334905cc223a7fcc4475ab5f1b72711888a52f9
solomonchidera <solochidera10@gmail.com>
2024-06-06 22:03:37 +0100

    A python funtion that uses the type annotation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 
 def add(a: float, b: float) -> float:
+    '''
+    Multiply two floats and return the result.
+
+    Parameters:
+    a: float
+    b: float
+
+    Return Value:
+    float: the result of two addition of floats
+    '''
     return a + b
