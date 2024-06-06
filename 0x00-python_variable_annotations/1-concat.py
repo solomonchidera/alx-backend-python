6d494af91503006914897fae516713a8da780eea
solomonchidera <solochidera10@gmail.com>
2024-06-06 22:39:24 +0100

    annotation type on function that concatenate two strings

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,9 +4,20 @@
 
 Function that takes two string and returns the concatenated version
 
-- concat string1 and string2 that gets concatenated
-
+Function:
+- concat string1 and string2 that gets concatenated and returns the result
 """
 
+
 def concat(string1: str, string2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Args:
+        string1 (str): The first string.
+        string2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
     return f'{string1}' + f'{string2}'
