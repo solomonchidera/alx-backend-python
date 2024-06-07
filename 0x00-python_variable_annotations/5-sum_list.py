5ebe9e4ae4256e9639f24562f23968296c55b34c
solomonchidera <solochidera10@gmail.com>
2024-06-07 02:02:02 +0100

    we all love ourselve...

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module Docs
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Function Docs
+    """
+    return sum(input_list)
