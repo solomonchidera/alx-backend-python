603cae89d16d78928f032cd734f793a77924f88f
solomonchidera <solochidera10@gmail.com>
2024-06-09 21:35:08 +0100

    Type annotation function that takes a list of float and interger and return the sum as float

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Module Docs
+"""
+from typing import List
+def sum_mixed_list(mxd_lst: List[float, int]) -> float:
+    """
+    Function Docs
+    """
+    return sum(mxd_lst)
