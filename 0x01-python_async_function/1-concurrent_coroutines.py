19034a2ba1e23d23f401b136a387d75e02cc55ea
solomonchidera <solochidera10@gmail.com>
2024-06-10 20:30:13 +0100

    just work ffs

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
-""" Module documentation """
+""" Parallel execution of multiple coroutines
+"""
 import asyncio
-import random
 from typing import List
-
-wait_random = __import__("0-basic_async_syntax").wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """doc func"""
-    listDelays = []
-    for _ in range(n):
-        listDelays.append(asyncio.create_task(wait_random(max_delay)))
-    return sorted(await asyncio.gather(*listDelays))
+    """ Function to call multiple coroutines 'n' times and
+        returns a list of delay time for each coroutine
+        Args:
+            n: iterations for coroutine
+            max_delay: maximum delay time to await
+                       coroutine
+        Return: list of delay time for each coroutine
+    """
+    delay = await asyncio.gather(*(wait_random(max_delay)
+                                 for _ in range(n)))
+    return sorted(delay)
