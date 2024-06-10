bec35f3a254707a1a7e35bbd8d2ebd852dd9753b
solomonchidera <solochidera10@gmail.com>
2024-06-10 20:24:34 +0100

    just work ffs

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
-"""
-Module Docs
-"""
+""" Module documentation """
 import asyncio
+import random
 from typing import List
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Function Docs
-    """
-    list_of_delays = []
+    """doc func"""
+    listDelays = []
     for _ in range(n):
-        list_of_delays.append(await wait_random(max_delay))
-    return sorted(list_of_delays)
+        listDelays.append(asyncio.create_task(wait_random(max_delay)))
+    return sorted(await asyncio.gather(*listDelays))
