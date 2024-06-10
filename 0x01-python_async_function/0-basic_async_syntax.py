14f29f2159283edfe9c97cf833f530b9e45c0479
solomonchidera <solochidera10@gmail.com>
2024-06-10 19:28:09 +0100

    Asyncio function that gets a random delay time and work based on the random number gotten

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -13,6 +13,6 @@ async def wait_random(max_delay: int = 10) -> float:
     Function Docs
     Generate a random delay time
     """
-    delay = random.uniform(0, max_delay)
+    delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
